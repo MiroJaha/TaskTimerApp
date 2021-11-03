a0171bc8204e0ab57764e5747f8b8d33b2dccab6
Mohammad Jaha <miro.tune@gmail.com>
2021-11-03 16:54:44 +0300

    Timer Update

--- app/src/main/java/com/example/tasktimerapp/MainActivity.kt
+++ app/src/main/java/com/example/tasktimerapp/MainActivity.kt
@@ -2,26 +2,49 @@ package com.example.tasktimerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val timer= findViewById<Chronometer>(R.id.timer)
-        val button= findViewById<Button>(R.id.button)
+        val chronometer= findViewById<Chronometer>(R.id.timer)
+        val start= findViewById<Button>(R.id.button)
         val stop= findViewById<Button>(R.id.button2)
+        var running= false
+        var pauseOffset: Long= 0
 
-        button.setOnClickListener{
-            timer.start()
+        chronometer.format = "Time: %s";
+        chronometer.base = SystemClock.elapsedRealtime();
+
+        chronometer.onChronometerTickListener = Chronometer.OnChronometerTickListener {
+            if ((SystemClock.elapsedRealtime() - chronometer.base) >= 10000) {
+                chronometer.base = SystemClock.elapsedRealtime();
+                Toast.makeText(this@MainActivity, "Bing!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        start.setOnClickListener{
+            if (!running) {
+                chronometer.base = SystemClock.elapsedRealtime() - pauseOffset;
+                chronometer.start();
+                running = true;
+            }
         }
 
         stop.setOnClickListener{
-            timer.stop()
-
+            if (running) {
+                chronometer.stop();
+                pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                running = false;
+            }
         }
-
+        //to reset the timer
+//        chronometer.base = SystemClock.elapsedRealtime();
+//        pauseOffset = 0;
     }
 }
