918d2160f0512703fca828806cf7c078a37c8239
Mohammad Jaha <miro.tune@gmail.com>
2021-11-03 16:07:06 +0300

    timer

--- app/src/main/java/com/example/tasktimerapp/MainActivity.kt
+++ app/src/main/java/com/example/tasktimerapp/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.tasktimerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val timer= findViewById<Chronometer>(R.id.timer)
+        val button= findViewById<Button>(R.id.button)
+        val stop= findViewById<Button>(R.id.button2)
+
+        button.setOnClickListener{
+            timer.start()
+        }
+
+        stop.setOnClickListener{
+            timer.stop()
+
+        }
 
     }
 }
