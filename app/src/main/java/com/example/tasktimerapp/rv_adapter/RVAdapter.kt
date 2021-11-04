e8d7760f2debbca733131d5b2c719968223b0682
Mohammad Jaha <miro.tune@gmail.com>
2021-11-04 17:06:09 +0300

    merging

---
+++ app/src/main/java/com/example/tasktimerapp/rv_adapter/RVAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.tasktimerapp.rv_adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tasktimerapp.databinding.ViewBinding
+import com.example.tasktimerapp.room.Data
+
+class RVAdapter(private val tasks: ArrayList<Data>) :
+    RecyclerView.Adapter<RVAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(val binding: ViewBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            ViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val taskName = tasks[position].taskName
+        val taskTime = tasks[position].taskTime
+
+        holder.binding.apply {
+            title.text = taskName
+        }
+    }
+
+    override fun getItemCount() = tasks.size
+}
