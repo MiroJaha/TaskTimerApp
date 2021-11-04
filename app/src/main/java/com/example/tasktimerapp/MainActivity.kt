841ab3d977ca2fa440cf5de374f12dcd2b336ce5
Mohammad Jaha <miro.tune@gmail.com>
2021-11-04 16:50:54 +0300

    deleted timer

--- app/src/main/java/com/example/tasktimerapp/MainActivity.kt
+++ app/src/main/java/com/example/tasktimerapp/MainActivity.kt
@@ -18,50 +18,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val chronometer = findViewById<Chronometer>(R.id.timer)
-        val start = findViewById<Button>(R.id.button)
-        val stop = findViewById<Button>(R.id.button2)
-        val showNumber = findViewById<TextView>(R.id.textView)
-        var running = false
-        var pauseOffset: Long = 0
 
-        chronometer.format = "Time: %s"
-        chronometer.base = SystemClock.elapsedRealtime()
-
-//        chronometer.onChronometerTickListener = Chronometer.OnChronometerTickListener {
-//            if ((SystemClock.elapsedRealtime() - chronometer.base) >= 10000) {
-//                chronometer.base = SystemClock.elapsedRealtime()
-//                Toast.makeText(this@MainActivity, "Bing!", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-
-        start.setOnClickListener {
-            if (!running) {
-                Log.d("MyTime","before ${chronometer.base} ${SystemClock.elapsedRealtime()} $pauseOffset")
-                chronometer.base = SystemClock.elapsedRealtime() - pauseOffset*10
-                Log.d("MyTime","after ${chronometer.base} ${SystemClock.elapsedRealtime()}")
-                chronometer.start()
-                running = true
-            }
-        }
-
-        stop.setOnClickListener {
-            if (running) {
-                chronometer.stop()
-                Log.d("MyTime","stop ${chronometer.base} ${SystemClock.elapsedRealtime()}")
-                pauseOffset = SystemClock.elapsedRealtime() - chronometer.base
-
-                //This Code Will Show Description About The Time EX(9 Seconds)
-                showNumber.text = chronometer.contentDescription
-
-                //This Code Will Show The Time Passed in Seconds
-                //(pauseOffset/1000).toString()
-
-                running = false
-            }
-        }
-        //to reset the timer
-//        chronometer.base = SystemClock.elapsedRealtime()
-//        pauseOffset = 0
     }
 }
