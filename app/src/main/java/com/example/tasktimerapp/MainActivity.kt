7daf51dabf245edf6d1a9466c51f3c1bca7e5bb7
Mohammad Jaha <miro.tune@gmail.com>
2021-11-05 16:34:21 +0300

    add another button

--- app/src/main/java/com/example/tasktimerapp/MainActivity.kt
+++ app/src/main/java/com/example/tasktimerapp/MainActivity.kt
@@ -3,14 +3,37 @@ package com.example.tasktimerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tasktimerapp.room.Data
+import com.example.tasktimerapp.rv_adapter.RVMainAdapter
+import kotlinx.coroutines.Dispatchers.IO
 
 class MainActivity : AppCompatActivity() {
 
     private val taskViewModel by lazy { ViewModelProvider(this).get(TaskViewModel::class.java) }
+    private lateinit var adapter: RVMainAdapter
+
+    private lateinit var rvItem: RecyclerView
+    private lateinit var tasksList: ArrayList<Data>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvItem= findViewById(R.id.rvItems)
+        tasksList= arrayListOf()
+
+        adapter= RVMainAdapter(tasksList)
+        rvItem.adapter= adapter
+        rvItem.layoutManager= LinearLayoutManager(this)
+
+        taskViewModel.getAllTasks().observe(this){
+            tasksList.clear()
+            tasksList.addAll(it)
+            adapter.notifyDataSetChanged()
+        }
+
 
     }
 }
--- app/src/main/java/com/example/tasktimerapp/rv_adapter/RVMainAdapter.kt
+++ app/src/main/java/com/example/tasktimerapp/rv_adapter/RVMainAdapter.kt
@@ -3,23 +3,22 @@ package com.example.tasktimerapp.rv_adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.tasktimerapp.databinding.ViewRvBinding
+import com.example.tasktimerapp.databinding.MainRvBinding
 import com.example.tasktimerapp.room.Data
 
 class RVMainAdapter(private val tasks: ArrayList<Data>) :
     RecyclerView.Adapter<RVMainAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(val binding: ViewRvBinding) : RecyclerView.ViewHolder(binding.root)
+    class ItemViewHolder(val binding: MainRvBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         return ItemViewHolder(
-            ViewRvBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            MainRvBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val taskName = tasks[position].taskName
-        val taskTime = tasks[position].taskTime
 
         holder.binding.apply {
             title.text = taskName
