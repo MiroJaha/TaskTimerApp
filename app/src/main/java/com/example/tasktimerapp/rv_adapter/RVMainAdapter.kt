7f2b44121956ed33b533e6ac5cbafaad6d57d916
Mohammad Jaha <miro.tune@gmail.com>
2021-11-06 00:40:26 +0300

    add edit class

--- app/src/main/java/com/example/tasktimerapp/rv_adapter/RVMainAdapter.kt
+++ app/src/main/java/com/example/tasktimerapp/rv_adapter/RVMainAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.tasktimerapp.rv_adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tasktimerapp.R
 import com.example.tasktimerapp.databinding.MainRvBinding
 import com.example.tasktimerapp.room.Data
 
-class RVMainAdapter(private val tasks: ArrayList<Data>) :
+class RVMainAdapter(private val context:Context,private val tasks: ArrayList<Data>) :
     RecyclerView.Adapter<RVMainAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(val binding: MainRvBinding) : RecyclerView.ViewHolder(binding.root)
@@ -22,6 +25,7 @@ class RVMainAdapter(private val tasks: ArrayList<Data>) :
 
         holder.binding.apply {
             title.text = taskName
+            ContextCompat.getColor(context, R.color.low)
         }
     }
 
