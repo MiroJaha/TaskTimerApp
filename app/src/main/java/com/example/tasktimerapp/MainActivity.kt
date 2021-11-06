46f0f42a348277ecbbc9a9383a8af45ff701745d
Mohammad Jaha <miro.tune@gmail.com>
2021-11-06 11:28:01 +0300

    edit main activity

--- app/src/main/java/com/example/tasktimerapp/MainActivity.kt
+++ app/src/main/java/com/example/tasktimerapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.widget.Chronometer
 import android.widget.ImageView
 import android.widget.ProgressBar
@@ -23,13 +24,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: RVMainAdapter
 
     private lateinit var rvItem: RecyclerView
-    private lateinit var tasksList: ArrayList<Data>
     private lateinit var taskName: TextView
     private lateinit var progressBar: ProgressBar
     private lateinit var timer: Chronometer
     private lateinit var reset: TextView
     private lateinit var addButton: ImageView
     private lateinit var showAllButton: ImageView
+
+    private lateinit var tasksList: ArrayList<Data>
+    private var running = false
+    private var pauseOffset: Long=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +46,6 @@ class MainActivity : AppCompatActivity() {
         reset= findViewById(R.id.reset)
         addButton= findViewById(R.id.bAdd)
         showAllButton= findViewById(R.id.showAll)
-        tasksList= arrayListOf()
 
         taskName.text = "No Task Started"
         timer.base = SystemClock.elapsedRealtime()
@@ -51,6 +54,7 @@ class MainActivity : AppCompatActivity() {
         rvItem.adapter= adapter
         rvItem.layoutManager= LinearLayoutManager(this)
 
+        tasksList= arrayListOf()
         taskViewModel.getAllTasks().observe(this){
             tasksList.clear()
             tasksList.addAll(it)
@@ -68,9 +72,43 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun startTimer(position: Int){
+        if (!running) {
+            progressBar.isIndeterminate = true
+            timer.base = SystemClock.elapsedRealtime() - pauseOffset
+            timer.start()
+            running = true
+        }
+        if (running) {
+            progressBar.isIndeterminate = false
+
+            progressBar.progress = 50
+            timer.stop()
+
+            pauseOffset = SystemClock.elapsedRealtime() - timer.base
+
+
+            //This Code Will Show The Time Passed in Seconds
+            //pauseOffset/1000
+
+            taskViewModel.updateTask(
+                Data(
+                    1,
+                    "999",
+                    "NoTime",
+                    tasksList[position].taskTime + pauseOffset / 1000,
+                    ""
+                )
+            )
+
+            running = false
+        }
+    }
+
     private fun addButtonsListeners() {
         reset.setOnClickListener{
             timer.base = SystemClock.elapsedRealtime()
+            pauseOffset = 0
         }
         addButton.setOnClickListener{
             startActivity(Intent(this,AddTaskActivity::class.java))
